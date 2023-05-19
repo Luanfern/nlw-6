41eaa2c79599262cebcf351279f2cc11ecc81a29
Luan Fernandes <luan@escribo.com>
2023-05-18 23:03:28 -0300

    auth user when change memories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,18 @@
-/* eslint-disable prettier/prettier */
 import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoute } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
 app.register(cors, {
-  origin: true
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app.register(authRoute)
